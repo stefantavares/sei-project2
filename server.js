eb2810618dc7787010eca0a777dc00fd0e9f14fd
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-12-15 15:13:21 -0800

    Add OAuth middleware

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+const isLoggedIn = require("./config/auth");
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// This will load our env variables
 require('dotenv').config();
+
+// This will connect us to the database
 require('./config/database');
+
+// require the passport module
+require('./config/passport');
 
 var app = express();
 
@@ -21,10 +31,24 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  // add req.user to res.locals
+  res.locals.user = req.user;
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', isLoggedIn, reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
