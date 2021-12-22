8df6ea64ff1594ad9de196f5cc3b25fe495a7bba
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-12-22 08:47:40 -0800

    More navigation buttons

--- controllers/tasklists.js
+++ controllers/tasklists.js
@@ -12,7 +12,6 @@ module.exports = {
 
 function index(req, res) {
     TaskList.find({ user: req.user }, function (err, tasklists) {
-        console.log(tasklists);
         res.render('tasklists/index', { tasklists });
     });
 }
@@ -26,7 +25,6 @@ function create(req, res) {
     const tasklist = new TaskList(req.body);
     tasklist.save(function (err) {
         if (err) return res.render('tasklists/new');
-        console.log(tasklist);
         res.redirect(`/tasklists`);
     });
 }
@@ -45,13 +43,11 @@ function deleteTaskList(req, res) {
 
 function edit(req, res) {
     TaskList.findOne({ _id: req.params.id }).then(function (tasklists) {
-        console.log('hiii: ', tasklists)
         res.render(`tasklists/edit`, { tasklists });
     });
 }
 
 function updateTaskList(req, res) {
-    console.log("RIGHT ROUTE")
     TaskList.findById(req.params.id, function (err, tasklists) {
         tasklists.title = req.body.title;
         tasklists.listDate = req.body.listDate;
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -11,7 +11,6 @@ function create(req, res) {
     TaskList.findById(req.params.id, function (err, tasklist) {
         tasklist.tasks.push(req.body);
         tasklist.save(function (err) {
-            console.log(req.body);
             res.redirect(`/tasklists/${tasklist._id}`);
         });
     });
